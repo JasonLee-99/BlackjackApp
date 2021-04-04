2ac8b735d4d98d6b896a8b6db1a757d30455e8cb
Jason Lee <jlee622@myseneca.ca>
2021-04-03 23:23:13 -0400

    linked game with menu

--- app/src/main/java/com/example/myapplication/MainMenu.java
+++ app/src/main/java/com/example/myapplication/MainMenu.java
@@ -14,7 +14,7 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main);
     }
 
     public void onPressLogout(View view) {
